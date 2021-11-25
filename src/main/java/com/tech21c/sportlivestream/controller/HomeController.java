5f03f677d568f0925326e11e1724e253a9e5281c
MinThantZin <developer.minthantzintun@gmail.com>
2021-11-25 23:42:23 +0630

    add runtime

--- src/main/java/com/tech21c/sportlivestream/controller/HomeController.java
+++ src/main/java/com/tech21c/sportlivestream/controller/HomeController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/")
 public class HomeController {
     @GetMapping(path = "/", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity showMesaage() {
